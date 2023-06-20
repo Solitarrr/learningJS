b6ca3239b7b8d90a0b4b932187f44d4c9600be09
Solitarrr <snill.ford19@gmail.com>
2023-06-20 21:59:58 +0300

    Commit for practice 2 task

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 "use strict";
 
+//First task
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+//Second task
+const personalMovieDB = {
+    "count": numberOfFilms,
+    "movies": {},
+    "actors": {},
+    "genres": [],
+    "privat": false
+};
